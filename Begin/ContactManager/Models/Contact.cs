c747860cdc745466d1e330f79e3d593cb8de8813
vdancevic <dancevic.vinko@gmail.com>
2019-11-07 18:35:18 +0100

    Task 3

---
+++ Begin/ContactManager/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ContactManager.Models;
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+        
+   
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+       
+    }
+}
